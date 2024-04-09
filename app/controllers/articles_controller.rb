f616906faeffd1f71cd1df17da3fc04ab88aa1a9
Mateo Loaiza <mateo@evolvesoftware.co>
2024-04-09 17:07:00 -0500

    Added strong parameters, create action to save articles and the html

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,20 @@ class ArticlesController < ApplicationController
     #This is an instance variable, needed so it can be called from the view
     @article = Article.find(params[:id])
   end
+
   def index
     @articles = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    # Both ways work
+    # redirect_to article_path(@article)
+    redirect_to @article
   end
 end
