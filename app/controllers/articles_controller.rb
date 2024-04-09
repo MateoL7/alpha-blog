ba0a198d850c8a8837fddc727b7e60947b081120
Mateo Loaiza <mateo@evolvesoftware.co>
2024-04-09 16:10:38 -0500

    Added index action with the table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,7 @@ class ArticlesController < ApplicationController
     #This is an instance variable, needed so it can be called from the view
     @article = Article.find(params[:id])
   end
+  def index
+    @articles = Article.all
+  end
 end
